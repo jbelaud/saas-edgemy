d5ff19e3ee9b2d98f698e9229b0275e9b525ce14
Jeremy BELAUD <jeremy.belaud@gmail.com>
2025-10-25 00:48:15 +0200

    fix: corriger imports date-fns v4 dans localizer\n\n- Utiliser named imports au lieu de default imports\n- date-fns v4 ne supporte plus les default exports\n- format, parse, startOfWeek, getDay maintenant en named imports

--- src/components/calendar/localizer.ts
+++ src/components/calendar/localizer.ts
@@ -1,8 +1,8 @@
 import { dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
+import { format } from "date-fns/format";
+import { parse } from "date-fns/parse";
+import { startOfWeek } from "date-fns/startOfWeek";
+import { getDay } from "date-fns/getDay";
 import { fr } from "date-fns/locale";
 
 const locales = { fr };
